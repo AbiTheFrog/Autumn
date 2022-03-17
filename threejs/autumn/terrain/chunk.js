482159a1a82de2838bf4a9b35d489964e3eda76e
Abigail Sokol <97188426+AbiTheFrog@users.noreply.github.com>
2022-03-17 14:11:13 -0500

    Decent water v. 2.0

--- threejs/autumn/terrain/chunk.js
+++ threejs/autumn/terrain/chunk.js
@@ -33,49 +33,14 @@ export default class Chunk {
     }
 
     constructor(scene, chunk, noise, ox, oy, oz, size, height, waterLevel){
-        // generate water
-        {
-            const wave = (x, z) => {
-                return noise.noise2D(ox + x, oz + z) * 0.3;
-            };
-
-            const geometry = new THREE.BufferGeometry();
-
-            const vertices = [];
-            
-            for(var x = 0; x < size; x++){
-                for(var z = 0; z < size; z++){
-                    vertices.push(...[
-                        x + 1, wave(x + 1, z), z,
-                        x, wave(x, z), z,
-                        x + 1, wave(x + 1, z + 1), z + 1,
-
-                        x, wave(x, z), z,
-                        x, wave(x, z + 1), z + 1,
-                        x + 1, wave(x + 1, z + 1), z + 1
-                    ]);
-                }
-            }
-
-            geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
-
-            geometry.computeVertexNormals();
-            geometry.normalizeNormals();
-
-            const water = new THREE.Mesh(geometry, waterMaterial);
-            
-            water.position.set(ox - 1, waterLevel - 0.1, oz);
-
-            scene.add(water);
-
-            this.water = water;
-        }
-
-        // generate land
-
+        // noise wrappers
         const getHeight = (i, j, max, min) => {
             return min + (((noise.noise2D(i / 64, j / 64) + noise.noise2D(i / 124, j / 124)) / 2 + 1) / 2) * (max - min - 1);
         }
+
+        const wave = (x, z) => {
+            return noise.noise2D(ox + x, oz + z) * 0.1;
+        };
         
         // make size x size x size chunk
         for(var i = 0; i < size; i++){
@@ -86,27 +51,35 @@ export default class Chunk {
                 for(; k < h; k++){
                     chunk[i][j][k] = 1;
                 }
+                for(; k < waterLevel; k++){
+                    chunk[i][j][k] = -2;
+                }
                 for(; k < height; k++){
-                    chunk[i][j][k] = 0;
+                    chunk[i][j][k] = -1;
                 }
             }
         }
 
         // logically get chunk position (as well as extrapolate to other chunks)
         const get = (chunk, x, y, z) => {
-            if(x < 0 || z < 0 || x >= size || z >= size){ return getHeight(x + ox, z + oz, height, 0) >= y ? 1 : 0; }
+            if(x < 0 || z < 0 || x >= size || z >= size){ return getHeight(x + ox, z + oz, height, 0) >= y ? 0 : 255; }
             if(y < 0 || y >= size){ return -1; }
             return chunk[x][z][y];
         }
 
         const check = (cur, chunk, x, y, z) => {
-            return get(chunk, x, y, z) == 0;
+            if(cur <= 200){
+                const val = get(chunk, x, y, z);
+                return val == -1 ? false : val > 200; }
+            else if(cur > 200){ return get(chunk, x, y, z) != cur; }
         }
 
         // object colors
-        const objColors = [0, [0.3764, 0.1960, 0.4078], [0, 0, 1]];
+        const objColors = [[1, 0, 0], [0.3764, 0.1960, 0.4078]];
 
         // generate mesh
+        const waterVertices = [];
+
         const vertices = [];
         const colors = [];
 
@@ -114,7 +87,8 @@ export default class Chunk {
             for(var y = 0; y < height; y++){
                 for(var z = 0; z < size; z++){
                     const cur = get(chunk, x, y, z);
-                    if(cur != 0){
+                    // render surface
+                    if(cur <= 200){
                         var n = 0;
 
                         if(check(cur, chunk, x + 1, y, z)){
@@ -204,29 +178,63 @@ export default class Chunk {
                         for(var i = 0; i < 2 * 3 * n; i++){
                             colors.push(...objColors[cur]);
                         }
+                    } else if(cur == 254){
+                        // render water surface
+                        if(check(cur, chunk, x, y + 1, z)){
+                            waterVertices.push(...[
+                                x + 1, y + wave(x + 1, z), z,
+                                x, y + wave(x, z), z,
+                                x + 1, y + wave(x + 1, z + 1), z + 1,
+
+                                x, y + wave(x, z), z,
+                                x, y + wave(x, z + 1), z + 1,
+                                x + 1, y + wave(x + 1, z + 1), z + 1
+                            ]);
+                        }
                     }
                 }
             }
         }
         
-        const geometry = new THREE.BufferGeometry();
-        geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
-        
-        geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
-        
-        geometry.computeVertexNormals();
-        geometry.normalizeNormals();
-        
-        const material = new THREE.MeshLambertMaterial({
-            vertexColors: THREE.VertexColors
-        });
-        const mesh = new THREE.Mesh(geometry, material);
+        // create land object
+        {
+            const geometry = new THREE.BufferGeometry();
+            geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(vertices), 3));
+            
+            geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
+            
+            geometry.computeVertexNormals();
+            geometry.normalizeNormals();
+            
+            const material = new THREE.MeshLambertMaterial({
+                vertexColors: THREE.VertexColors
+            });
+            const mesh = new THREE.Mesh(geometry, material);
 
-        mesh.position.set(ox, oy, oz);
-        
-        scene.add(mesh);
-        
-        this.mesh = mesh;
+            mesh.position.set(ox, oy, oz);
+            
+            scene.add(mesh);
+            
+            this.mesh = mesh;
+        }
+
+        // create water object
+        {
+            const geometry = new THREE.BufferGeometry();
+
+            geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(waterVertices), 3));
+
+            geometry.computeVertexNormals();
+            geometry.normalizeNormals();
+
+            const water = new THREE.Mesh(geometry, waterMaterial);
+            
+            water.position.set(ox - 1, oy + 0.7, oz);
+
+            scene.add(water);
+
+            this.water = water;
+        }
     }
 
     unload(scene){
